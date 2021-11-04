2af8c4b80bf369275821a39d8831e39bd708a9bd
matCh00 <mateusz.chalik.g@gmail.com>
2021-11-05 00:39:54 +0100

    lab1 5.0

--- lab1/main.py
+++ lab1/main.py
@@ -17,28 +17,29 @@ def shutdown():
 
 def render(time):
 
-    # glClear(GL_COLOR_BUFFER_BIT)
-    #
-    # simpleRectangle()
-    #
-    # drawRectangle(-90, -90, 40, 40)
-    #
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    # simpleTriangle()
+    # drawTriangle(-100, -100, 100)
+
+    # drawRectangle(-50, 50, 100, 100)
+
     # random.seed(8)
     # c1 = random.randint(0, 1)
     # c2 = random.randint(0, 1)
     # c3 = random.randint(0, 1)
     # d = random.uniform(0.5, 2.5)
-    # deformatedTriangle(0, -90, 40, 40, d, c1, c2, c3)
+    # deformatedTriangle(0, 0, 40, 40, d, c1, c2, c3)
 
-    drawFractal1(-100, -100, 200, 200, 5)
+    # drawFractal1(-100, 100, 200, 200, 5)
 
-    # drawFractal2(0, -80, 200, 8, 5)
+    drawFractal2(0, -90, 30, math.pi/2.0, 0.8, math.pi/6.0, 12)
 
     glFlush()
 
 
 # prostokąt z 6 punktów
-def simpleRectangle():
+def simpleTriangle():
     glClear(GL_COLOR_BUFFER_BIT)
 
     glBegin(GL_TRIANGLES)
@@ -62,29 +63,51 @@ def simpleRectangle():
     glFlush()
 
 
-# prostokąt z 4 punktów
+# prostokąt z 4 punktów, (x,y) to współżędne lewego górnego rogu
 def drawRectangle(x, y, a, b):
 
+    if x < -100:
+        x = -100
+    if y > 100:
+        y = 100
+    if x + b > 100:
+        x = 100 - b
+    if y - a < -100:
+        y = -100 + a
+
     glBegin(GL_TRIANGLES)
-    glColor3f(1.0, 1.0, 1.0)
+    glColor3f(1.0, 1.0, 0.0)
     glVertex2f(x, y)
     glColor3f(0.0, 1.0, 0.0)
-    glVertex2f(x, y + b)
+    glVertex2f(x, y - b)
     glColor3f(1.0, 0.0, 0.0)
-    glVertex2f(x + a, y + b)
+    glVertex2f(x + a, y - b)
     glEnd()
 
     glBegin(GL_TRIANGLES)
-    glColor3f(1.0, 1.0, 1.0)
+    glColor3f(1.0, 1.0, 0.0)
     glVertex2f(x, y)
-    glColor3f(1.0, 0.0, 0.0)
-    glVertex2f(x + a, y + b)
     glColor3f(0.0, 0.0, 1.0)
     glVertex2f(x + a, y)
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex2f(x + a, y - b)
     glEnd()
 
 
-# losowy trójkąt
+# trójkąt, (a,b) to współżędne lewego dolnego rogu
+def drawTriangle(a, b, d):
+
+    glBegin(GL_TRIANGLES)
+    glColor3f(0.8, 0.5, 0.1)
+    glVertex2f(a, b)
+    glColor3f(0.2, 0.0, 0.4)
+    glVertex2f(a + d, b)
+    glColor3f(1.0, 0.0, 0.3)
+    glVertex2f(a, d + b)
+    glEnd()
+
+
+# losowy trójkąt, (x,y) to współżędne lewego dolnego rogu
 def deformatedTriangle(x, y, a, b, d, c1, c2, c3):
 
     glBegin(GL_TRIANGLES)
@@ -98,33 +121,46 @@ def deformatedTriangle(x, y, a, b, d, c1, c2, c3):
 
 
 # dywan Sierpińskiego
-def drawFractal1(x, y, a, b, deep):
+def drawFractal1(x, y, a, b, level):
 
-    rectangles = [(x, y, a, b)]
-    drawRectangle(x, y, a, b)
+    k = 3.0
 
-    for _ in range(deep):
-        newRectangles = []
+    if level > 0:
+        new_x = x + (b / k)
+        new_y = y - (a / k)
+        new_a = a / k
+        new_b = b / k
+        drawRectangle(new_x, new_y, new_a, new_b)
 
-        for rect in rectangles:
-            x, y = rect[0], rect[1]
-            a = rect[2] / 3
-            b = rect[3] / 3
-            drawRectangle(x + a, y + b, a, b)
-            newRectangles.append((x, y, a, b))
-            newRectangles.append((x + a, y, a, b))
-            newRectangles.append((x + 2 * a, y, a, b))
-            newRectangles.append((x, y + b, a, b))
-            newRectangles.append((x + 2 * a, y + b, a, b))
-            newRectangles.append((x, y + 2 * b, a, b))
-            newRectangles.append((x + a, y + 2 * b, a, b))
-            newRectangles.append((x + 2 * a, y + 2 * b, a, b))
-
-        rectangles = newRectangles
+        if level > 1:
+            for i in range(0, 3):
+                for j in range(0, 3):
+                    if i == 1 and j == 1:
+                        continue
+                    drawFractal1(x + (new_b * i), y - (new_a * j), new_a, new_b, level - 1)
 
 
-def drawFractal2(x, y, a, b, level, deep=5):
-    pass
+def fractal2(x, y, len, angle):
+
+    glBegin(GL_LINES)
+    glVertex2f(x, y)
+    glVertex2f(x + len * math.cos(angle), y + len * math.sin(angle))
+    glEnd()
+
+
+# inny fraktal
+def drawFractal2(x, y, len, angle, len_div, angle_dif, depth):
+
+    fractal2(x, y, len, angle)
+
+    if depth <= 0:
+        return
+
+    # lewa gałąź
+    drawFractal2(x + len * math.cos(angle), y + len * math.sin(angle), len * len_div, angle - angle_dif, len_div, angle_dif, depth - 1)
+
+    # prawa gałąź
+    drawFractal2(x + len * math.cos(angle), y + len * math.sin(angle), len * len_div, angle + angle_dif, len_div, angle_dif, depth - 1)
 
 
 def update_viewport(window, width, height):
