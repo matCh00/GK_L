b564eae7014c7d1017bcbaf8fed4709d180ebc71
matCh00 <mateusz.chalik.g@gmail.com>
2022-01-08 18:47:02 +0100

    lab7 3.0

--- lab7/main.py
+++ lab7/main.py
@@ -18,23 +18,27 @@ def compile_shaders():
         #version 330 core
 
         in vec4 position;
-
+        out vec4 vertex_color;
+        
         uniform mat4 M_matrix;
         uniform mat4 V_matrix;
         uniform mat4 P_matrix;
 
         void main(void) {
             gl_Position = P_matrix * V_matrix * M_matrix * position;
+            vertex_color = vec4(0.1, 0.1, 0.9, 1.0);
         }
     """
 
     fragment_shader_source = """
         #version 330 core
-
+        
+        in vec4 vertex_color;
         out vec4 color;
 
         void main(void) {
             color = vec4(0.7, 0.7, 0.7, 1.0);
+            color = vertex_color;
         }
     """
 
